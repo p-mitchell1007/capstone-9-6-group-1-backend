0e8bc4b89d380892b44cd05cab251cdd7c43a6d0
Your Name <you@example.com>
2023-10-31 14:42:24 -0400

    added my table and sample data for posts

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,13 @@ DROP DATABASE IF EXISTS reflections;
 CREATE DATABASE reflections;
 
 \c reflections;
+
+DROP TABLE IF EXISTS _posts
+
+CREATE TABLE _posts (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER REFERENCES Users(id),
+  post_made DATE,
+  title TEXT,
+  content TEXT
+);
---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+\c reflections;
+
+INSERT INTO _posts (id, user_id, post_made, title, content)
+VALUES
+  (1, 1, '2023-10-30', 'Struggling with Parenting Stress', 'Any advice on managing stress during pregnancy?'),
+  (2, 2, '2023-10-30', 'Looking for Postpartum Depression Support', 'Feeling overwhelmed after childbirth, need someone to talk to.'),
+  (3, 1, '2023-10-31', 'Recommendation for Therapist', 'Can anyone suggest a good therapist for postpartum depression in my area?'),
+  (4, 3, '2023-11-01', 'Coping Strategies for New Parents', 'Share your best coping strategies for dealing with lack of sleep and baby-related stress.');
