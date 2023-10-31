eac367ac2121ab6a8f33654c177c5691670d73f9
Michelle Harley <chell.hx@gmail.com>
2023-10-30 22:01:21 -0400

    post schema and seed basic, post query made, and post controller started

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+\c reflections;
+
+INSERT INTO 
+    posts (title, content)
+VALUES
+('Day 3', 'Baby was born 3 days ago. I haven''t gotten an ounce of sleep since the night before I had the baby. Baby does not sleep through the night. I can not stop crying. Someone help.'),
+('Little Bundle of Joy', 'Some nights are hard but I just remember he''s my little bundle of joy.'),
+('Postpartum', 'Postpartum depression is common for us all. Men and women feel the fear, frustrastions, stress, anxiety and tiredness after having a child. Once we acknowledge those feelings, we can then start to heal and work through them');
