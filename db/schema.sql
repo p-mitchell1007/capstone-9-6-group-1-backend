a6fc7ef204e1d31683f66f6f8f7a8cc5961c8393
winterbelle <blancaaltamirano@pursuit.org>
2023-11-05 11:52:53 -0500

    dm table and message-receiver table

--- db/schema.sql
+++ db/schema.sql
@@ -12,3 +12,24 @@ CREATE TABLE posts (
     title VARCHAR(200),
     content TEXT
 );
+
+DROP TABLE IF EXISTS direct_message;
+
+CREATE TABLE direct_message (
+    id SERIAL PRIMARY KEY,
+    message_subject TEXT,
+    message_content TEXT,
+    send_date TIMESTAMP,
+    parent_id INTEGER FOREIGN KEY,
+    sender_id INTEGER FOREIGN KEY
+);
+
+DROP TABLE IF EXISTS message_reciever;
+
+CREATE TABLE message_reciever (
+    id SERIAL PRIMARY KEY,
+    sender_id INTEGER FOREIGN KEY,
+    receiver_id INTEGER FOREIGN KEY,
+    message_id INTEGER FOREIGN KEY,
+    reply_at TIMESTAMP,
+);
