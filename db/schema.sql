c5853c952bdd8864727f43a840f365f1e188b708
Your Name <you@example.com>
2023-11-12 11:31:23 -0500

    Johns changes

--- db/schema.sql
+++ db/schema.sql
@@ -3,38 +3,54 @@ CREATE DATABASE reflections;
 
 \c reflections;
 
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  fname TEXT,
+  lname TEXT,
+  email TEXT,
+  phone VARCHAR(15),
+  city VARCHAR(50),
+  homestate VARCHAR(50),
+  password_hash VARCHAR(255),
+  profile_img TEXT,
+  created_at TIMESTAMP DEFAULT now()
+);
+
+
 DROP TABLE IF EXISTS posts;
 
 CREATE TABLE posts (
-  id INTEGER PRIMARY KEY,
-  user_id INTEGER REFERENCES Users(id),
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
   post_made DATE,
   title TEXT,
-  content TEXT
+  content VARCHAR
 );
 
 DROP TABLE IF EXISTS comments;
 
 CREATE TABLE comments (
-  id INTEGER PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES Users(id),
   post_id INTEGER REFERENCES Posts(id),
   comment_made DATE,
-  content TEXT
+  content VARCHAR
 );
 
-DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  fname TEXT,
-  lname TEXT,
-  email TEXT,
-  phone VARCHAR(15),
-  user_id INTEGER REFERENCES Users(id),
-  city VARCHAR(50),
-  homestate VARCHAR(50),
-  password_hash VARCHAR(255),
-  profile_img TEXT,
-  created_at TIMESTAMP DEFAULT
-);
+-- CREATE TABLE users (
+--   id INTEGER PRIMARY KEY,
+--   fname TEXT,
+--   lname TEXT,
+--   email TEXT,
+--   phone VARCHAR(15),
+--   user_id INTEGER REFERENCES Users(id),
+--   city VARCHAR(50),
+--   homestate VARCHAR(50),
+--   password_hash VARCHAR(255),
+--   profile_img TEXT,
+--   created_at TIMESTAMP DEFAULT
+-- );
