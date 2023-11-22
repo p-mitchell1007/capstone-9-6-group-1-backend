dcb00f5a70fab918a4d4da24d5058802369dbb7f
Raymond <raymondjohnson@pursuit.org>
2023-11-21 20:17:07 -0500

    completed routes and queries for posts

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,11 +3,16 @@ const router = express.Router();
 
 const {
     getAllPosts,
+    getPostByPostId,
+    createPost,
+    deletePost,
+    // updatePost
 } = require("../queries/postsQueries");
 
+/* --- GET ALL POSTS --- */
 router.get("/", async (req, res) => {
     const getPosts = await getAllPosts();
-
+    
     if (!Array.isArray(getPosts)) {
         return res.status(500).json({ Error: "Server Error. Please Try Again."})
     } else {
@@ -15,4 +20,40 @@ router.get("/", async (req, res) => {
     }
 })
 
+/* --- GET A SINGLE POSTS --- */
+router.get("/:id", async (req, res) => {
+    const postId = req.params.id;
+    const postById = await getPostByPostId(postId)
+    res.json(postById)
+})
+
+/* --- CREATE A NEW POSTS --- */
+router.post("/", async (req, res) => {
+    const {userId, postMade, title, content } = req.body
+    const newPost = await createPost(userId, postMade, title, content)
+    res.json(newPost)
+})
+
+/* --- UPDATE A POST --- */
+// router.put("/:id", async (req, res) => {
+//     const postId = req.params.id;
+//     const postById = await updatePost(postId)
+//     res.json(postById)
+// })
+
+/* --- DELETE A POST --- */
+router.delete("/:id", async (req, res) => {
+    const postId = req.params.id;
+    const deletedPost = await deletePost(postId)
+    res.json(deletedPost)
+    // if(alert('Confirm Delete?')){
+
+    // } else {
+    //     alert('Action Cancelled!')
+    // }
+})
+
 module.exports = router;
+
+
+
